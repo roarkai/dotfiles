1fc38069eb54d74f74cb0b0a5bfadd3232cddebf
rk <hiroark@foxmail.com>
2022-12-28 15:21:25 +0800

    modified the install file

--- dotfile_install.sh
+++ dotfile_install.sh
@@ -10,10 +10,10 @@ rm ~/.tmux.conf
 rm ~/.p10k.zsh
 
 # create soft link 
-ln -s -f ~/dotfile/.tmux.conf ~/.tmux.conf
-ln -s -f ~/dotfile/.p10k.zsh ~/.p10k.zsh
-ln -s -f ~/dotfile/.zshrc ~/.zshrc
-ln -s -f ~/dotfile/.vimrc ~/.vimrc
+ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s -f ~/dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -s -f ~/dotfiles/.zshrc ~/.zshrc
+ln -s -f ~/dotfiles/.vimrc ~/.vimrc
 
 # reconfigure the programs above
 source ~/.zshrc 
