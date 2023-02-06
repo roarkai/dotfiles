c2c14edbd1da2d1041487c83926219668e064341
roark_mac0 <hiroark@foxmail.com>
2023-02-07 00:37:05 +0800

    add sh file for Ubuntu and linux version of zshrc

---
+++ dotfile_ser_install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh
+
+# Dont link DS_Store files
+# DS_store can be create by iOS, and should be deleted before installing the dotfile on other machine
+find . -name ".DS_Store" -exec rm {} \;
+
+rm ~/.zshrc
+rm ~/.vimrc
+rm ~/.tmux.conf
+rm ~/.p10k.zsh
+
+# create soft link 
+ln -s -f ~/dotfiles_ser/.tmux.conf.ser ~/.tmux.conf # adapt for theme in ubuntu
+ln -s -f ~/dotfiles_ser/.p10k.zsh ~/.p10k.zsh
+ln -s -f ~/dotfiles_ser/.zshrc.ser ~/.zshrc # add conf for conda
+ln -s -f ~/dotfiles_ser/.vimrc ~/.vimrc
+
+# reconfigure the programs above
+source ~/.zshrc 
+
+echo "zshrc, vimrc, tmux.conf have been replaced!"
+echo "Now, you need to finish the configure by activate those apps"
